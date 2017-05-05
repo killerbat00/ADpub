3da54b7e1e2d7fdf0fe4d0dbf2300e804f22873b
brian houston morrow <bhm@brianmorrow.net>
2017-05-05 04:29:41 -0500

    Placeholder for BreweryDB API key.
    Replace the placehoolder text in the `BREWERY_KEY` variable with your BreweryDB API key before you deploy this application with chalice.

---
+++ chalicelib/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This package contains only the Brewery DB API key. It is necessary to provide
+an alternate method for this key entry when using chalice so that secrets
+aren't improperly shared.
+"""
+
+BREWERY_KEY = "<YOUR_BREWERY_DB_API_APP_KEY_HERE>"
